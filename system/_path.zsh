24b95bef200541031e58af5fae48fd25836d5233
Yoshio HANAWA <hanawa-y@klab.com>
2015-06-09 19:08:01 +0900

    Fix $PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$PHPENV_ROOT/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/bin:$PHPENV_ROOT/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
