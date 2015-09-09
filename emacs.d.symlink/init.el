5c9f0c48bd0cd7bfe163915a59e43536cef8ae14
Yoshio HANAWA <y@hnw.jp>
2015-09-09 22:16:43 +0900

    Add setting for Emacs Mac Port

--- emacs.d.symlink/init.el
+++ emacs.d.symlink/init.el
@@ -253,6 +253,20 @@
   (setq browse-url-generic-program "open")
   ;; Ctrl+SpaceをSpotLight＆ことえりに取られないようにする
   (exec-if-bound (mac-add-ignore-shortcut '(control ? )))
+  ;; Emacs Mac Port 用設定
+  ;; ミニバッファで入力する際に自動的にASCIIにする
+  (when (fboundp 'mac-auto-ascii-mode)
+    (mac-auto-ascii-mode 1))
+  ;; カーソルの色を変える
+  (when (fboundp 'mac-input-source)
+    (defun my-mac-selected-keyboard-input-source-chage-function ()
+      "英語のときはカーソルの色をdim grayに、日本語のときはbrownにします."
+      (let ((mac-input-source (mac-input-source)))
+        (set-cursor-color
+         (if (string-match "\\.Roman$" mac-input-source)
+             my-roman-cursor-color my-japanese-cursor-color))))
+    (add-hook 'mac-selected-keyboard-input-source-change-hook
+              'my-mac-selected-keyboard-input-source-chage-function))
   )
 
 ;; hide menubar/toolbar
