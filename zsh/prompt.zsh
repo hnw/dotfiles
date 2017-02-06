372c8be86c26bdce96d935692a260e9ddddb4455
Yoshio HANAWA <y@hnw.jp>
2017-02-06 21:17:52 +0900

    gitリポジトリの状態表示を速度改善

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,41 +9,32 @@ else
   git="/usr/bin/git"
 fi
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
 git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
+    declare st
+    st=$($git status --porcelain 2>/dev/null) || return
+    if [[ $st == "" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+        echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+        echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
     fi
-  fi
 }
 
 git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
+    declare ref
+    ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+    echo "${ref#refs/heads/}"
 }
 
 need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
+    declare unpushed
+    unpushed=$($git cherry -v @{upstream} 2>/dev/null) || return
+    if [[ $unpushed == "" ]]
+    then
+        echo " "
+    else
+        echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+    fi
 }
 
 ruby_version() {
