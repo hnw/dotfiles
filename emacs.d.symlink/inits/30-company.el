08e6ff5b39ef2d85a9a5c907ecdc3152e9ffdd32
Yoshio HANAWA <y@hnw.jp>
2018-11-16 01:28:40 +0900

    company-mode用設定を別ファイルに分離

---
+++ emacs.d.symlink/inits/30-company.el
@@ -0,0 +1,35 @@
+(require 'company)                                   ; load company mode
+
+;;; Possible improvements
+
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-idle-delay .3)                         ; decrease delay before autocompletion popup shows
+(setq company-echo-delay 0)                          ; remove annoying blinking
+(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
+
+;;; Remap company-select-next and company-select-previous to C-n and C-p
+;;; via: https://emacs.stackexchange.com/questions/2988/how-to-remap-companys-select-next-and-select-previous-keys
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
+;;; Color customization
+
+(custom-set-faces
+ '(company-preview
+   ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common
+   ((t (:inherit company-preview))))
+ '(company-tooltip
+   ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection
+   ((t (:background "steelblue" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection)))))
